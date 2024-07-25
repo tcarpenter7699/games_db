217951fc770f7eff0a552ad2fcab11e95c5feee9
tcarpenter7699 <tcarpenter7699@gmail.com>
2024-07-25 01:42:59 -0500

    added game id route, morgan and body-parser

--- api/game.js
+++ api/game.js
@@ -3,7 +3,7 @@ const gameRouter = express.Router();
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-// /api/game/
+// GET /api/game/
 gameRouter.get('/', async (req, res) => {
     try{
     const games = await prisma.game.findMany({
@@ -17,5 +17,37 @@ gameRouter.get('/', async (req, res) => {
     res.sendStatus(500)
     }
 });
+
+// GET /api/game/:id
+gameRouter.get('/:id', async (req, res) => {
+    try{
+    const game = await prisma.game.findUnique({
+        where: {
+            id: parseInt(req.params.id),
+        },
+        include : {
+            developer: true,
+        }
+    });
+    res.send(game);
+    } catch(err){
+    console.log(err);
+    res.sendStatus(500)
+    }
+});
+
+// POST /api/game/
+gameRouter.post("/", async (req, res) => {
+    try{
+    const newGame = await prisma.game.create({
+        data:{
+            //GET DATA FROM BODY
+        }
+    });
+    }catch(err){
+    console.log(err);
+    res.sendStatus(500)
+    }
+})
 
 module.exports = gameRouter;
--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const express = require('express')
 const app = express()
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 8080;
+
+//logs each request on the server
+app.use(morgan('dev'));
+
+//parses the request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//logs out the body to the console
+app.use((req, res, next) => {
+    console.log("<___BODY LOGGER START___>");
+    console.log(req.body);
+    console.log("<___BODY LOGGER END___>");
+    next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
